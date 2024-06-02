2a65072020ec41d775a35deb74839d2559610cda
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2024-06-02 20:39:43 +0200

    Sketch out skills component

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
 import React from "react";
+
+const icons = {
+  frontend: {
+    HTML: "/icons/icons8-html.svg",
+    CSS: "/icons/icons8-css.svg",
+    JavaScript: "/icons/icons8-javascript.svg",
+    TypeScript: "/icons/icons8-typescript.svg",
+    React: "/icons/icons8-react.svg",
+    ["Next.js"]: "/icons/icons8-nextjs.svg",
+    tailwind: "/icons/icons8-tailwind-css.svg",
+  },
+  backend: {
+    ["Node.js"]: "/icons/icons8-nodejs.svg",
+  },
+};
 
 function Skills() {
   return (
-    <div className="container py-4 text-4xl font-medium tracking-wide">
-      <h2>My skills</h2>
+    <div className="container py-4">
+      <div className="">
+        <h2 className="text-2xl font-medium tracking-wide">My skills</h2>
+        <h3>Frontend</h3>
+        <ul className="flex flex-col gap-2">
+          {Object.entries(icons.frontend).map(([tech, svgUrl]) => {
+            return (
+              <li key={tech} className="flex items-center gap-4 rounded-md px-2">
+                <img alt="" src={svgUrl} className="h-8 w-8"></img>
+                <p className="text-center">{tech}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
