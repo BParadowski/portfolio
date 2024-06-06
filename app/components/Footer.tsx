c4aaddca9520dde06e31ed9c4b3a2c614f6b4923
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2024-06-06 08:33:51 +0200

    Change layout a bit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="sticky bg-stone-100">
+      <div className="container">
+        <div className="flex items-center py-6">
+          <p className="px-4 py-2 uppercase leading-none">Bartosz Paradowski</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const inter = Roboto({ subsets: ["latin"], weight: ["500", "400", "700"] });
 
@@ -17,9 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} grid grid-rows-[auto_1fr_auto]`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
